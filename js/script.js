21c511922587e771f8e31c07b769385f316239ee
Mstufing <mstufingms11@gmail.com>
2023-06-01 15:52:01 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// ? Task #1
+// ? Enharmonic Equivalents
+
+// function getEquivalent(note) {
+// 	let obj = {
+// 		"C#": "Db",
+// 		"D#": "Eb",
+// 		"F#": "Gb",
+// 		"G#": "Ab",
+// 		"A#": "Bb",
+// 		"Db": "C#",
+// 		"Eb": "D#",
+// 		"Gb": "F#",
+// 		"Ab": "G#",
+// 		"Bb": "A#"
+// 	}
+// 	return obj[note]
+// }
+
+// let btn = document.querySelector('.btn');
+
+// btn.onclick = () => {
+//     let firstInp = document.querySelector('.inp1');
+//     let res = document.querySelector('.result');
+
+//     res.innerHTML = getEquivalent(firstInp.value);
+// }
+
